cf1e4a1c795e7332a74bbc89300cb0c432c1e571
Xing Cui <cxcui@cn.ibm.com>
2025-08-11 09:16:38 +0800

    first

---
+++ codes/ems-jsp/src/main/java/com/baizhi/EmsJspApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsJspApplication.class, args);
+    }
+
+}
